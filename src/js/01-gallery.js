26cdc801b269e24db6653f67b1a6ccc3c9bc55ed
Ihor <sadovenko555@gmail.com>
2023-05-11 23:07:26 +0300

    Add script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const qwert = 56;
+
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const createGalleryItemMarkup = makeCardsMurkup(galleryItems);
+
+galleryList.insertAdjacentHTML('afterbegin', createGalleryItemMarkup);
+
+function makeCardsMurkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, original, preview }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
